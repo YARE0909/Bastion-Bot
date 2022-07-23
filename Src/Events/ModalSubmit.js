e13618115007c33a28c782a36e3c1a359720245e
unknown <rosyrdas@gmail.com>
2022-07-23 14:33:15 +0530

    add modal submit

---
+++ Src/Events/ModalSubmit.js
@@ -0,0 +1,17 @@
+const {
+    InteractionType
+} = require('discord.js')
+module.exports = {
+    name: 'interactionCreate',
+    /**
+     * 
+     * @param {import('discord.js').Client} client 
+     * @param {import('discord.js').ModalSubmitInteraction} interaction 
+     * @param {*} container 
+     */
+    run: async (client, interaction, container) => {
+        if (interaction.type === InteractionType.ModalSubmit) {
+            // continue the code, THIS IS ONLY CALLED WHEN THE MODAL IS SUBMITTED!!
+        }
+    }
+}
